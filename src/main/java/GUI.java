1b03ee1dd3c9332e95f37b22a3e119372d6ede30
Dmitriy <volga19171@gmail.com>
2021-09-26 20:23:56 +0400

    add exception handling in GUI

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -8,6 +8,8 @@ public class GUI implements ActionListener {
     String file;
     String directory;
     String fileName;
+    Integer key;
+    String errorMessage = "Неверно введен ключ! Допускается только целочисленный формат.";
 
     JFrame frame = new JFrame("Окно шифрования/дешифрования файла");
     JFrame parentFrame = new JFrame();
@@ -16,7 +18,12 @@ public class GUI implements ActionListener {
     JButton decryptionButton = new JButton("Расшифровать файл");
     JButton selectFile = new JButton("Выбрать файл");
     JTextField keyText = new JTextField("Введите ключ шифрования", 16);
+    JTextField result = new JTextField();
     JFileChooser fileChooser = new JFileChooser();
+
+    public static void main(String[] args) {
+        new GUI();
+    }
 
     public GUI() {
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
@@ -25,13 +32,15 @@ public class GUI implements ActionListener {
         decryptionButton.addActionListener(this);
         keyText.addActionListener(this);
         selectFile.addActionListener(this);
+        result.setEditable(false);
 
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 5, 30));
-        panel.setLayout(new GridLayout(4, 1));
+        panel.setLayout(new GridLayout(5, 1));
         panel.add(selectFile);
         panel.add(keyText);
         panel.add(encryptionButton);
         panel.add(decryptionButton);
+        panel.add(result);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,16 +48,28 @@ public class GUI implements ActionListener {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new GUI();
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == encryptionButton) {
-            Encryption.encryptFile(file, directory + "\\ENCRYPTED_" + fileName, Integer.parseInt(keyText.getText()));
+            try {
+                key = Integer.parseInt(keyText.getText());
+                Encryption.encryptFile(file, directory + "\\ENCRYPTED_" + fileName, key);
+                result.setText("Файл успешно зашифрован!");
+            } catch (NumberFormatException exception) {
+                result.setText(errorMessage);
+            } catch (NullPointerException nullPointerException) {
+                result.setText("Укажите файл!");
+            }
         } else if (e.getSource() == decryptionButton) {
-            Decryption.decryptFile(file, directory + "\\DECRYPTED_" + fileName, Integer.parseInt(keyText.getText()));
+            try {
+                key = Integer.parseInt(keyText.getText());
+                Decryption.decryptFile(file, directory + "\\DECRYPTED_" + fileName, key);
+                result.setText("Файл успешно расшифрован!");
+            } catch (NumberFormatException exception) {
+                result.setText(errorMessage);
+            } catch (NullPointerException nullPointerException) {
+                result.setText("Укажите файл!");
+            }
         } else if (e.getSource() == selectFile) {
             fileChooser.setDialogTitle("Выбрать файл");
             fileChooser.showOpenDialog(parentFrame);
